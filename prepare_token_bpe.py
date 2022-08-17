f9a2ff4869a324255d999799cd7260dad9217f9e
iotfsktm <fdaus@gmail.com>
2022-08-17 19:59:22 +0800

    update model path

--- prepare_token_bpe.py
+++ prepare_token_bpe.py
@@ -18,4 +18,4 @@ tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=
 ])
 
 # Save files to disk
-tokenizer.save_model(".", "sampletoken")
+tokenizer.save_model("./models/", "sampletoken")
